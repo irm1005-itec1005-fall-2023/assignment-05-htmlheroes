4d83e268880f6906f432d6a11f0b966a1943c990
ionaleigh <ionaleigh228@gmail.com>
2023-12-09 21:27:20 -0500

    LAUNCHnolink

--- script.js
+++ script.js
@@ -1,82 +1,68 @@
-//
-//  JS File
-//  YOU CAN REMOVE ALL OF THIS CODE AND START FRESH
-//
+//event listeners for the nav link
+document.getElementById("contactLink").addEventListener("click", function(){
+  openConPopup();
+});
 
-//
-// Variables
-//
+//func to open popup window
+function openConPopup() {
+  //console.log(contactPopup);
+  const popupName = `PopupWindow_contactPopup`;
+  const popup = window.open('', popupName, 'width=300,height=410');
 
-// Constants
-const appID = "app";
-const headingText = "Develop. Preview. Ship.";
-const headingTextIcon = "🚀";
-const projectDueDate = "8 December 2023 11:59";
+  //content for window
+  popup.document.write(`
+      <title>Contact Us</title>
+      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap">
+      <div class="popupTot">
+      <div class="popupHeader">
+          <h2>Contact Us!!</h2>
+      </div>
+      <div class="popupForm">
+          <form action="#" method="post">
+              <label for="name">Name:</label>
+              <input type="text" id="name" name="name" class="input-box" required><br>
 
-// Variables
-let countdownDate = new Date(projectDueDate);
+              <label for="email">Email:</label>
+              <input type="email" id="email" name="email" class="input-box" required><br>
 
-// DOM Elements
-let appContainer = document.getElementById(appID);
+              <label for="message">Message:</label><br>
+              <textarea id="message" name="message" rows="4" class="input-box" required></textarea><br>
 
-//
-// Functions
-//
-
-function calculateDaysLeft(countdownDate) {
-  const now = new Date().getTime();
-  const countdown = new Date(countdownDate).getTime();
-
-  console.log(countdown);
-
-  const difference = (countdown - now) / 1000;
-
-
-  // Countdown passed already
-  if (difference < 1) {
-    return null;
-  }
-
-
-  const days = Math.floor(difference / (60 * 60 * 24));
-
-  return days;
+              <button class="submit" onclick="window.close()">Submit</button>
+          </form>
+      </div>
+      </div>
+  `);
+  popup.document.close();
 }
 
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
-  }
+  //event listeners for the nav link
+document.getElementById("aboutLink").addEventListener("click", function(){
+  openAbPopup();
+});
 
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  const daysLeft = calculateDaysLeft(countdownDate);
-  let headingTextCalculated = headingText;
+//func to open popup window
+function openAbPopup() {
+  //console.log("clicked!");
+  const popupName = `PopupWindow_aboutPopup`;
+  const popup = window.open('', popupName, 'width=410,height=320');
 
-  if (daysLeft > 1) {
-    headingTextCalculated = headingTextCalculated.concat(
-      " In ",
-      daysLeft.toString(),
-      " days "
-    );
-  }else if (daysLeft === 1) {
-    headingTextCalculated = headingTextCalculated.concat(
-      " Tomorrow"
-    );
-  }
-
-  h1.textContent = headingTextCalculated.concat(headingTextIcon);
-  appContainer.appendChild(h1);
-
-  // Init complete
-  console.log("App successfully initialised");
+  //content for window
+  popup.document.write(`
+      <title>About Us</title>
+      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap">
+      <div class="abTot">
+          <p>Welcome to the HTMLHeroes mini arcade! I promise it looks bad on purpose, I was going for an early 2000s "cool math games" type of vibe. Select one of our (4)?? games on our main page to start playing! (The rest are just placeholders to make it look nicer). Enjoy :D!</p>
+      <div class="blocks">
+          <img src="./images/etc/Untitled (13).png" alt="question block icon">
+          <img src="./images/etc/Untitled (14).png" alt="question block icon">
+          <img src="./images/etc/Untitled (15).png" alt="question block icon">
+          <img src="./images/etc/Untitled (16).png" alt="question block icon">
+      </div>
+      </div>
+  `);
+  popup.document.close();
 }
-
-//
-// Inits & Event Listeners
-//
-
-inititialise();
+//event handlers at bottom of page
